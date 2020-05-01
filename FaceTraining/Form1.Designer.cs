b89a28950e3d7e44bfc41f46fe903f467a3a7c2b
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-02 00:13:06 +0300

    add project

---
+++ FaceTraining/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FaceTraining
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.buttonBaslat = new System.Windows.Forms.Button();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.textBoxIsim = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.Location = new System.Drawing.Point(12, 12);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(637, 592);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // buttonBaslat
+            // 
+            this.buttonBaslat.Location = new System.Drawing.Point(717, 12);
+            this.buttonBaslat.Name = "buttonBaslat";
+            this.buttonBaslat.Size = new System.Drawing.Size(114, 39);
+            this.buttonBaslat.TabIndex = 3;
+            this.buttonBaslat.Text = "Tarama ve Algılama Başlat";
+            this.buttonBaslat.UseVisualStyleBackColor = true;
+            this.buttonBaslat.Click += new System.EventHandler(this.ButtonBaslat_Click);
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(717, 160);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(114, 36);
+            this.buttonKaydet.TabIndex = 4;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.ButtonKaydet_Click);
+            // 
+            // textBoxIsim
+            // 
+            this.textBoxIsim.Location = new System.Drawing.Point(756, 107);
+            this.textBoxIsim.Name = "textBoxIsim";
+            this.textBoxIsim.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIsim.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(717, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "İsim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 632);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIsim);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.buttonBaslat);
+            this.Controls.Add(this.cameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Emgu.CV.UI.ImageBox cameraBox;
+        private System.Windows.Forms.Button buttonBaslat;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.TextBox textBoxIsim;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
